1562b37e4c017b719e8ffffc402d85600c5f506d
David Sommerseth <dazo@users.sourceforge.net>
2009-03-05 13:32:40 +0100

    Rewrote xmlout.py to use xml.dom.minidom

--- rteval/xmlout.py
+++ rteval/xmlout.py
@@ -1,65 +1,84 @@
 #!/usr/bin/python -tt
 import os
 import sys
+import xml.dom.ext
+import xml.dom.minidom
+
 
 class XMLOut(object):
     '''Class to create XML output'''
-    def __init__(self, filename, encoding='UTF-8', standalone=True):
-        self.filename = filename
+    def __init__(self, roottag, attr, encoding='UTF-8'):
+        self.xmldoc = xml.dom.minidom.Document()
+
+        self.xmlroot = self.xmldoc.createElement(roottag)
+        self.__add_attributes(self.xmlroot, attr)
+        self.currtag = self.xmlroot
+        self.level = 0
+        self.closed = False
         self.encoding = encoding
-        if standalone:
-            self.standalone = 'yes'
-        else:
-            self.standalone = 'no'
-        self.indent = 0
-        self.indentchar = '\t'
-        self.blocktags = []
-        self.handle = open(filename, 'w')
-        self.handle.write('<?xml version="1.0" encoding="%s" standalone="%s"?>\n'
-                          % (self.encoding, self.standalone))
+
+
+    def __add_attributes(self, node, attr):
+        if attr is not None:
+            for k, v in attr.iteritems():
+                node.setAttribute(k, str(v))
+
 
     def close(self):
-        if self.indent:
-            raise RuntimeError, "XMLOut: open blocks at close: %s" % " ".join(self.blocktags)
-        self.handle.close()
+        if self.level > 0:
+            raise RuntimeError, "XMLOut: open blocks at close"
+        self.xmldoc.appendChild(self.currtag)
+        self.closed = True
+
+
+    def Write(self, file, xslt = None):
+        if not self.closed:
+            raise RuntimeError, "XMLOut: XML file is not closed"
+
+        if xslt == None:
+            # If no XSLT template is give, write raw XML
+            file.write(self.xmldoc.toxml(encoding=self.encoding))
+            return
+
 
     def __del__(self):
-        if self.indent != 0:
-            raise RuntimeError, "XMLOut: open blocks at close: %s" % " ".join(self.blocktags)
+        if self.level > 0:
+            raise RuntimeError, "XMLOut: open blocks at close"
 
-    def __indent(self):
-        return self.indentchar * self.indent
 
-    def __attributes(self, attributes):
-        astr = ''
-        if attributes:
-            if isinstance(attributes, dict):
-                for a in attributes.keys():
-                    astr = astr + ' %s="%s"' % (a, attributes[a])
-            elif isinstance(attributes, list) or isinstance(attributes, tuple):
-                astr = ' ' + ' '.join(attributes)
-        return astr
+    def openblock(self, tagname, attributes=None):
+        ntag = self.xmldoc.createElement(tagname);
+        self.__add_attributes(ntag, attributes)
+        self.currtag.appendChild(ntag)
+        self.currtag = ntag
+        self.level += 1
 
-    def openblock(self, tag, attributes=None):
-        astr = self.__attributes(attributes)
-        self.handle.write('%s<%s%s>\n' % (self.__indent(), tag, astr))
-        self.indent += 1
-        self.blocktags.append(tag)
 
     def closeblock(self):
-        self.indent -= 1
-        tag = self.blocktags.pop()
-        self.handle.write('%s</%s>\n' % (self.__indent(), tag))
+        if self.level == 0:
+            raise RuntimeError, "XMLOut: no open tags to close"
+        self.currtag = self.currtag.parentNode
+        self.level -= 1
+
 
     def taggedvalue(self, tag, value, attributes=None):
-        astr = self.__attributes(attributes)
-        self.handle.write('%s<%s%s>%s</%s>\n' % (self.__indent(), tag, astr, value, tag))
+        ntag = self.xmldoc.createElement(tag)
+        self.__add_attributes(ntag, attributes)
+        ntag.appendChild(self.xmldoc.createTextNode(str(value)))
+        self.currtag.appendChild(ntag)
+
 
 
 if __name__ == '__main__':
-    x = XMLOut('test.xml')
-    x.openblock('test', {'version':"1.0"})
+    x = XMLOut('rteval', {'version':"1.0"})
+    x.openblock('test', {'testattr': "yes", 'works': "hopefully"})
     x.taggedvalue('foo', 'bar')
     x.taggedvalue('baz', 'frooble', {'version':'1.0'})
+    x.openblock('subtag', {'level': "1"})
+    x.openblock('subtag', {'level': "2"})
+    x.taggedvalue('value','another value')
+    x.closeblock()
+    x.closeblock()
     x.closeblock()
     x.close()
+    x.Write(sys.stdout)
