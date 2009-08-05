XaF/rteval

commit 3b21ff811a792becdbc59ad7800940df3befe09f

Improved possibility to get more debug info, and to run tests without executing SQL queries

server/rtevaldb.py
@@ -28,9 +28,10 @@
 from database import Database
 from xmlparser import XMLSQLparser
 
-def register_report(xslt, xmldata, filename):
+def register_report(xslt, xmldata, filename, debug=False, noaction=False):
     dbc = Database(host="rtserver.farm.hsv.redhat.com", database="rteval",
                    user="xmlrpc", password="RTeval")
+    dbc = Database(database="rteval", debug=debug, noaction=noaction)
     parser = XMLSQLparser(xslt, xmldata)
 
     systems = parser.GetSQLdata('systems')

server/database.py
@@ -30,7 +30,11 @@ import psycopg2
 import types
 
 class Database(object):
-    def __init__(self, host=None, port=None, user=None, password=None, database=None):
+    def __init__(self, host=None, port=None, user=None, password=None, database=None,
+                 noaction=False, debug=False):
+        self.noaction = noaction
+        self.debug = debug
+
         dsnd = {}
         if host is not None:
             dsnd['host'] = host
@@ -46,7 +50,7 @@ class Database(object):
             dsnd['dbname'] = database
 
         dsn = " ".join(["%s='%s'" %(k,v) for (k,v) in dsnd.items()])
-        self.conn = psycopg2.connect(dsn)
+        self.conn = not self.noaction and psycopg2.connect(dsn) or None
 
 
     def INSERT(self, sqlvars):
@@ -87,7 +91,7 @@ class Database(object):
             )
 
         # Get a database cursor
-        curs = self.conn.cursor()
+        curs = not self.noaction and self.conn.cursor() or None
 
         #
         # Loop through all records and insert them into the database
@@ -102,11 +106,15 @@ class Database(object):
             for i in range(0, len(sqlvars['fields'])):
                 values[sqlvars['fields'][i]] = rec[i]
 
+            if self.debug:
+                print "SQL QUERY: ==> %s" % (sqlstub % values)
+
             # Do the INSERT query
-            curs.execute(sqlstub, values)
+            if not self.noaction:
+                curs.execute(sqlstub, values)
 
             # If a return value for the INSERT is defined, catch that one
-            if sqlvars['returning']:
+            if not self.noaction and sqlvars['returning']:
                 # The psycopg2 do not handle INSERT INTO ... RETURNING column queries, so we can only use
                 # this on tables with oid and do the look up that way
                 vls = {"table": sqlvars['table'], 'colname': sqlvars['returning'], 'oid': str(curs.lastrowid)}
@@ -115,11 +123,12 @@ class Database(object):
             else:
                 results.append(True)
 
-        curs.close()
+        if not self.noaction:
+            curs.close()
         return results
 
     def SELECT(self, table, fields, joins=None, where=None):
-        curs = self.conn.cursor()
+        curs = not self.noaction and self.conn.cursor() or None
 
         # Query
         try:
@@ -129,7 +138,13 @@ class Database(object):
                 joins and "%s" % joins or "",
                 where and "WHERE %s" % " AND ".join(["%s = %%(%s)s" % (k,k) for (k,v) in where.items()] or "")
                 )
-            curs.execute(sql, where)
+            if self.debug:
+                print "SQL QUERY: ==> %s" % (sql % where)
+            if not self.noaction:
+                curs.execute(sql, where)
+            else:
+                # If no action is setup (mainly for debugging), return empty result set
+                return {"table": table, "fields": [], "records": []}
         except Exception, err:
             raise Exception, "** SQL ERROR *** %s\n** SQL ERROR ** Message: %s" % ((sql % where), str(err))
 
@@ -147,15 +162,19 @@ class Database(object):
             records.append(values)
 
         curs.close()
+        if debug:
+            print "database::SELECT() result ** Fields: %s\nRecords: %s" % (fields, records)
         return {"table": table, "fields": fields, "records": records}
 
     def COMMIT(self):
         # Commit the work
-        self.conn.commit()
+        if not self.noaction:
+            self.conn.commit()
 
     def ROLLBACK(self):
         # Abort / rollback the current work
-        self.conn.rollback()
+        if not self.noaction:
+            self.conn.rollback()
 
 
     def GetValue(self, dbres, recidx, field):

server/xmlrpc_API1.py
@@ -34,10 +34,11 @@ import rtevaldb
 
 
 class XMLRPC_API1():
-    def __init__(self, dataroot="/tmp/rteval", nodbaction=False):
+    def __init__(self, dataroot="/tmp/rteval", debug=False, nodbaction=False):
         # Some defaults
         self.dataroot = dataroot
         self.fnametrans = string.maketrans("/\\", "::") # replace path delimiters in filenames
+        self.debug = debug
         self.nodbaction = nodbaction
 
     def __mkdatadir(self, dirpath):
@@ -89,12 +90,15 @@ class XMLRPC_API1():
         self.__mkdatadir(self.dataroot + '/reports/' + clientid)
         fname = self.__getfilename('reports/' + clientid,'report.xml', False)
         xmldoc.saveFormatFileEnc(fname,'UTF-8',1)
+        if self.debug:
+            print "Copy of report: %s" % fname
 
         # Register the report into a database and return the rteval run id
+        (syskey, rterid) = rtevaldb.register_report('xmlparser.xsl', xmldoc, fname,
+                                                        debug=self.debug, noaction=self.nodbaction)
         if not self.nodbaction:
-            (syskey, rterid) = rtevaldb.register_report('xmlparser.xsl', xmldoc, fname)
-        else:
             rterid = 999999999 # Fake ID when no database registration is done
+
         return rterid
 
 

commit 25cfad6c750573929e24c1d5980426d2465d55bf

Cleaned up a DB connection regression
This was introduced in commit 3b21ff811a792becdbc59ad7800940df3befe09f
by a mistake.

server/rtevaldb.py
@@ -30,8 +30,7 @@ from xmlparser import XMLSQLparser
 
 def register_report(xslt, xmldata, filename, debug=False, noaction=False):
     dbc = Database(host="rtserver.farm.hsv.redhat.com", database="rteval",
-                   user="xmlrpc", password="RTeval")
-    dbc = Database(database="rteval", debug=debug, noaction=noaction)
+                   user="xmlrpc", password="RTeval", debug=debug, noaction=noaction)
     parser = XMLSQLparser(xslt, xmldata)
 
     systems = parser.GetSQLdata('systems')