2c147002151aa931bb67f763d2f36eece86a8179
Clark Williams <williams@redhat.com>
2009-10-12 13:44:17 -0500

    version bump to 1.5
    updated option processing, changed stats to use less memory,
    changed debug output to prefix with module name

--- rteval/rteval.py
+++ rteval/rteval.py
@@ -57,7 +57,7 @@ import rtevalMailer
 
 class RtEval(object):
     def __init__(self):
-        self.version = "1.4"
+        self.version = "1.5"
         self.load_modules = []
         self.workdir = os.getcwd()
         self.inifile = None
