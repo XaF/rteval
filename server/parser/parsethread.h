65342d2ed541c48ffd7bfb41f04118b7c99771f1
David Sommerseth <davids@redhat.com>
2009-10-21 07:49:39 +0200

    Added another missing file

---
+++ server/parser/parsethread.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2009 Red Hat Inc.
+ *
+ * David Sommerseth <davids@redhat.com>
+ *
+ * Takes a standardised XML document (from parseToSQLdata()) and does
+ * the database operations based on that input
+ *
+ * This application is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef _PARSETHREAD_H
+#define _PARSETHREAD_H
+
+typedef enum { jbNONE, jbAVAIL } jobStatus;
+
+/**
+ * This struct is used for sending a parse job to a worker thread via POSIX MQ
+ */
+typedef struct {
+	jobStatus status;                  /**< Info about if job information*/
+        unsigned int submid;               /**< Work info: Numeric ID of the job being parsed */
+        char filename[4092];               /**< Work info: Full filename of the report to be parsed*/
+} parseJob_t;
+
+
+void *parsethread(void *thrargs);
+
+#endif
