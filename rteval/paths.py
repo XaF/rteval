f981c19f286565617b3ce19e44da562f677b172a
Raphaël Beamonte <raphael.beamonte@gmail.com>
2013-03-25 15:50:30 -0400

    Introduce a new paths.py file to contain the installation paths.
    This new paths.py file contains the paths to the configuration, the
    rteval scripts, libs and data. It is used to know exactly where the
    files are currently, depending on the installation parameters given
    to setup.py. By default, this file contains paths allowing to use
    rteval without having to install it.
    
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

---
+++ rteval/paths.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright 2013        Raphaël Beamonte <raphael.beamonte@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#   For the avoidance of doubt the "preferred form" of this code is one which
+#   is in an open unpatent encumbered format. Where cryptographic key signing
+#   forms part of the process of creating an executable the information
+#   including keys needed to generate an equivalently functional executable
+#   are deemed to be part of the source code.
+
+RTEVAL_DIR_CONF        = '.'
+RTEVAL_DIR_SCRIPTS     = '.'
+RTEVAL_DIR_DATA        = './rteval'
+RTEVAL_DIR_LIB         = './rteval'
